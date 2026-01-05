bb87d1c742081d44cdc9b2e0b10ca436cf03e9dd
Moadret <martin99ode@gmail.com>
2026-01-05 14:59:57 +0100

    sprites and collisions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import sys
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -10,14 +14,48 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")    
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
               return
+        
+        updatable.update(dt)
+        
+        for thing in asteroids:
+            if thing.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+
+
         screen.fill("black")
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+
+
     
 
 
